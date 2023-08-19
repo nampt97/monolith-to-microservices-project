fcbf3c84f44b4025ba198f6c6f22eb1d7e4932bd
nampt010897 <nam.thanhphan97@gmail.com>
2023-08-19 23:41:17 +0700

    update

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'phannam',
+  'password': '123456789',
+  'database': 'postgres',
+  'host': 'cdr.cv32nctucne7.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'test-350883800889-dev',
+  'url': 'http://localhost:8080',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
